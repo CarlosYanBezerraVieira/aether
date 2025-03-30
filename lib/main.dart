d93c429f29a501db72d076355ccaf291a14508b2
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2025-03-29 23:08:35 -0300

    chore: implemetend provider with instance of http client and move code for home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:aether/body.dart';
-import 'package:aether/footer.dart';
-import 'package:aether/header.dart';
-import 'package:aether/ui/app_color.dart';
-import 'package:aether/widgets/indicator_of_scroll.dart';
-import 'package:aether/widgets/show_temperature.dart';
+import 'package:aether/home_page.dart';
+import 'package:aether/core/network/http_adapter.dart';
+import 'package:aether/core/network/htttp_client.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -15,58 +13,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double width = MediaQuery.of(context).size.width;
-    final double height = MediaQuery.of(context).size.height;
-    final bodyHeight = height * 0.5;
-    final bottomHeight = height * 0.3;
-    final horizontalSpace = -(width * 0.05);
-    const indicatorScrollPadding = 16.0;
-    final positionVerticalOfBody =
-        (height - bodyHeight - bottomHeight - indicatorScrollPadding);
-    return CupertinoApp(
-      debugShowCheckedModeBanner: false,
-      home: Container(
-        color: AppColor.catedralColor,
-        child: SafeArea(
-          child: Stack(
-            children: [
-              Positioned(
-                right: horizontalSpace,
-                left: horizontalSpace,
-                top: positionVerticalOfBody,
-                child: Body(
-                  height: bodyHeight,
-                  width: width,
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      const Header(),
-                      const SizedBox(
-                        height: 32,
-                      ),
-                      ShowTemperature(horizontalSpace: horizontalSpace)
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      const IndicatorOfScroll(
-                        selectedIndex: 0,
-                      ),
-                      Footer(
-                        height: bottomHeight,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return MultiProvider(providers: [
+      Provider<HttpClient>(
+          create: (_) => HttpAdapter(),
+          dispose: (context, adapter) => (adapter as HttpAdapter).dispose()),
+    ], child: const HomePage());
   }
 }
