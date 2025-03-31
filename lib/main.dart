fb35f11b58eaf288e6a9dcc8c2a93d651bddbfb4
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2025-03-31 14:03:37 -0300

    chore: add home controler and service weather in providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
-import 'package:aether/home_page.dart';
+import 'package:aether/home/home_page.dart';
 import 'package:aether/core/network/http_adapter.dart';
 import 'package:aether/core/network/htttp_client.dart';
 import 'package:aether/repositories/weather_repository.dart';
 import 'package:aether/repositories/weather_repository_impl.dart';
+import 'package:aether/services/weather_service.dart';
+import 'package:aether/services/weather_service_impl.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
+import 'home/home_controller.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+
   runApp(const MainApp());
 }
 
@@ -18,18 +23,31 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      Provider<HttpClient>(
-        create: (_) => HttpAdapter(
-            apiKey: dotenv.env['API_KEY'] ?? '',
-            baseUrl: dotenv.env['BASE_URL'] ?? ''),
-        dispose: (context, adapter) => (adapter as HttpAdapter).dispose(),
-      ),
-      Provider<WeatherRepository>(
-        create: (context) => WeatherRepositoryImpl(
-          httpClient: context.read<HttpClient>(),
-        ),
-      ),
-    ], child: const HomePage());
+    return MultiProvider(
+        providers: [
+          Provider<HttpClient>(
+            create: (_) => HttpAdapter(
+                apiKey: dotenv.env['API_KEY'] ?? '',
+                baseUrl: dotenv.env['BASE_URL'] ?? ''),
+            dispose: (context, adapter) => (adapter as HttpAdapter).dispose(),
+          ),
+          Provider<WeatherRepository>(
+            create: (context) => WeatherRepositoryImpl(
+              httpClient: context.read<HttpClient>(),
+            ),
+          ),
+          Provider<WeatherService>(
+            create: (context) => WeatherServiceImpl(
+              weatherRepository: context.read<WeatherRepository>(),
+            ),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => HomeController(
+              weatherService: context.read<WeatherService>(),
+            ),
+          ),
+        ],
+        child: const CupertinoApp(
+            debugShowCheckedModeBanner: false, home: HomePage()));
   }
 }
