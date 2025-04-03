ca76bdb8cfe9506b359d0d13537be08ed45a181b
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2025-04-03 15:50:34 -0300

    style: adjustment in the color of the cathedral

--- lib/core/ui/utils/app_color.dart
+++ lib/core/ui/utils/app_color.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const Color catedralColor = Color(0xFFd2dbb5);
+  static const Color catedralColor = Color(0xFFc2caa8);
   static const Color coliseuColor = Color(0xFFe0dcc2);
   static const Color estatuaColor = Color(0xFF0e065a);
   static const Color textPrimaryColor = Color(0xFF18133f);
