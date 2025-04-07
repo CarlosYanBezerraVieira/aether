e8d0f55f89c67ae18924d5669bd298abd1a548f7
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2025-04-07 18:45:14 -0300

    feat: add page view in body

--- lib/modules/home/widgets/body.dart
+++ lib/modules/home/widgets/body.dart
@@ -1,19 +1,29 @@
-import 'package:aether/core/ui/widgets/animated_video.dart';
+import 'package:aether/modules/animated_video/animated_video.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:video_player/video_player.dart';
 
 class Body extends StatelessWidget {
+  final Function(int) onPageChanged;
+  final List<VideoPlayerController> controllers;
   final double width;
   final double height;
-  const Body({super.key, required this.width, required this.height});
+  const Body(
+      {super.key,
+      required this.width,
+      required this.height,
+      required this.onPageChanged,
+      required this.controllers});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: width,
-      height: height,
-      child: const AnimatedVideo(
-        path: 'assets/places/catedral.mp4',
-      ),
-    );
+        width: width,
+        height: height,
+        child: PageView(
+          onPageChanged: onPageChanged,
+          children: controllers.map((controller) {
+            return AnimatedVideo(controller: controller);
+          }).toList(),
+        ));
   }
 }
